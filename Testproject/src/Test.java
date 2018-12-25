e98a9ff5bcc2913c85f91f99d0dd1913474c6a73
Pallavi Bharat Naikwade <pallavi.naikwade13@gmail.com>
2018-12-25 13:22:13 +0530

    create class

--- Testproject/src/Test.java
+++ Testproject/src/Test.java
@@ -1,4 +1,11 @@
 
 public class Test {
+	public static void main(String args[]){
 
-}
+    System.out.println("I am learnig devops");
+    System.out.println("1. Github");
+    System.out.println("2. Jekins");
+    System.out.println("3. pupet");
+    
+System.out.println("hello welcome to github account");
+}}
